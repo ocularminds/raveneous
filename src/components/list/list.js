4aa830db43eefc05563bfcc547188fd903239f80
Festus B. Jejelowo <mail.festus@gmail.com>
2020-05-09 00:23:44 +0100

    refactored code for repetition

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -3,10 +3,21 @@ import './list.css';
 import Business from '../business/business';
 
 class BusinessList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.display = this.display.bind(this);
+  }
+
+  display(business, index) {
+    return <Business business={business} key={index} />;
+  }
+
   render() {
-    return (<div className="BusinessList">
-        <Business/>
-  </div>);
+    return (
+      <div className="BusinessList">
+        {this.props.businesses.map((e, i) => this.display(e, i))}
+      </div>
+    );
   }
 }
 export default BusinessList;
